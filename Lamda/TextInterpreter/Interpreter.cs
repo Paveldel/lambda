ef0548ed610508f2fb7f68b9fe682da285a680c7
paveldel <pavel.deleersnyder@student.howest.be>
2025-12-21 14:32:57 +0100

    make interpreter more robust

--- Lamda/TextInterpreter/Interpreter.cs
+++ Lamda/TextInterpreter/Interpreter.cs
@@ -6,12 +6,10 @@ public class Interpreter
 {
     public Node Interpret(string lamda)
     {
-        return lamda[0] switch
-        {
-            '(' => InterpretCall(lamda),
-            'λ' => InterpretDefinition(lamda),
-            _ => InterpretLiteral(lamda)
-        };
+        if (lamda[0] == '(') return InterpretCall(lamda);
+        if (lamda[0] == 'λ') return InterpretDefinition(lamda);
+        if (lamda.Contains('(')) return InterpretCallWithParts(lamda);
+        return InterpretLiteral(lamda);
     }
 
     private Node InterpretLiteral(string lamda)
@@ -29,9 +27,53 @@ public class Interpreter
 
     private Node InterpretCall(string lamda)
     {
+        int endOfSpacing = EndOfPart(lamda);
+        if (endOfSpacing != lamda.Length - 1) return InterpretCallWithParts(lamda);
         int split = SpaceForCall(lamda);
+        if (split != -1) return InterpretCallWithSpace(lamda, split);
+        lamda = lamda.Substring(1, lamda.Length - 2);
+        return Interpret(lamda);
+    }
+
+    private Node InterpretCallWithParts(string lamda)
+    {
+        string[] parts = GetParts(lamda);
+        Node node = new Call(Interpret(parts[0]), Interpret(parts[1]));
+        for (int i = 2; i < parts.Length; i++)
+        {
+            node = new Call(node, Interpret(parts[i]));
+        }
+        return node;
+    }
+
+    private string[] GetParts(string lamda)
+    {
+        List<string> parts = [];
+        while (lamda.Length > 0)
+        {
+            if (lamda[0] == '(')
+            {
+                int endOfPart = EndOfPart(lamda);
+                string part = lamda.Substring(0, endOfPart + 1);
+                lamda = lamda.Remove(0, part.Length);
+                parts.Add(part);
+            }
+            else
+            {
+                int call = lamda.IndexOf('(');
+                if (call == -1) call = lamda.Length;
+                string part = lamda.Substring(0, call);
+                lamda = lamda.Remove(0, part.Length);
+                parts.Add(part);
+            }
+        }
+        return parts.ToArray();
+    }
+
+    private Node InterpretCallWithSpace(string lamda, int split)
+    {
         return new Call(
-            Interpret(lamda.Substring(1, split - 1)), 
+            Interpret(lamda.Substring(1, split - 1)),
             Interpret(lamda.Substring(split + 1, lamda.Length - split - 2)));
     }
 
@@ -45,6 +87,18 @@ public class Interpreter
             if (subCalls > 0) subCalls--;
             else return i;
         }
-        throw new Exception($"Couldn't interpret Call \"{call}\"");
+        return -1;
+    }
+    
+    private int EndOfPart(string call)
+    {
+        int subPart = 1;
+        for (int i = 1; i < call.Length; i++)
+        {
+            if (call[i] == '(') subPart++;
+            if (call[i] == ')') subPart--;
+            if (subPart == 0) return i;
+        }
+        return -1;
     }
 }
