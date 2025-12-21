002e31204e55c867af6735175ee0c72fe662e510
paveldel <pavel.deleersnyder@student.howest.be>
2025-12-21 22:13:05 +0100

    allow parameters to be overshadowed in drawings

--- Lamda/drawer/DrawDefinition.cs
+++ Lamda/drawer/DrawDefinition.cs
@@ -6,11 +6,22 @@ public class DrawDefinition(string parameter, Drawable body) : Drawable
 {
     public void Draw(Bitmap canvas, (int x, int y) offset, Dictionary<string, int> parameters)
     {
-        (int x, int y) = GetBoundingBox();
+        string replacement = Guid.NewGuid().ToString();
+        if (parameters.ContainsKey(parameter))
+        {
+            parameters[replacement] = parameters[parameter];
+            parameters.Remove(parameter);
+        }
+        
+        (int x, _) = GetBoundingBox();
         parameters[parameter] = parameters.Count * 2;
         canvas.DrawHorizontal(offset.x, parameters[parameter], x);
         body.Draw(canvas, offset, parameters);
         parameters.Remove(parameter);
+        
+        if (!parameters.ContainsKey(replacement)) return;
+        parameters[parameter] = parameters[replacement];
+        parameters.Remove(replacement);
     }
 
     public (int x, int y) GetBoundingBox()
