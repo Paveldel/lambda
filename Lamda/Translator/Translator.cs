818b123c399305195a2e3639db318a8db4ecc1a7
paveldel <pavel.deleersnyder@student.howest.be>
2025-12-21 12:22:46 +0100

    write translator to turn AST back into expression

---
+++ Lamda/Translator/Translator.cs
@@ -0,0 +1,16 @@
+﻿using Lamda.AST;
+
+namespace Lamda.Translator;
+
+public class Translator
+{
+    public string Translate(Node node)
+    {
+        return node switch
+        {
+            Literal literal => literal.Name,
+            Definition definition => $"λ{definition.Parameter}.{Translate(definition.Body)}",
+            Call call => $"({Translate(call.Function)} {Translate(call.Value)})"
+        };
+    }
+}
