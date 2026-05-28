8085030680ed516cb1742d9956ca4f58a8ef56be
paveldeleeersnyder <pavel.deleersnyder@student.howest.be>
2026-05-28 11:22:25 +0200

    correct rename

--- Lambda/Program.cs
+++ Lambda/Program.cs
@@ -6,15 +6,13 @@ using Lamda.Translator;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-string expression = "λn.λf.n(λc.λa.λb.c b(λx.a (b x)))(λx.λy.x)(λx.x)f";
+string expression = "(λc2.λc3.(λc4. c3 c4 (λn. n (λf.λb. b f (λx. x)) (λb.λf. c3 (b f)) c3) c4) (c2 c2)) (λf.λx. f (f x)) (λf.λx. f (f (f x)))";
 
 Node tree = new Interpreter().Interpret(expression);
+// for (int k = 0; k < 500; k++)
+// {
+//     tree = tree.Reduce();
+// }
 new Drawer().Draw(tree);
-for (int k = 0; k < 5; k++)
-{
-    string betweenResult = new Translator().Translate(tree);
-    Console.WriteLine(betweenResult);
-    tree = tree.Reduce();
-}
 string result = new Translator().Translate(tree);
 Console.WriteLine(result);
